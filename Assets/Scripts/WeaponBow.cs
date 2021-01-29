75b0f1f8825c175fa46b11956fbb9c8f4c58e2e5
Jordan Rowland <46544691+rowlanddeep@users.noreply.github.com>
2021-01-28 21:04:24 -0500

    changed bow to hold-to-fire

--- Assets/Scripts/WeaponBow.cs
+++ Assets/Scripts/WeaponBow.cs
@@ -11,7 +11,10 @@ public class WeaponBow : MonoBehaviour
     public float fireSpeed = 0.5f;
     public float projectileSpeed = 7.0f;
     public float despawnTime = 5.0f;
-    private bool canFire = true;
+
+    public float holdTime = 0.5f;
+    private float holdStartTime;
+    private bool holding = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +24,28 @@ public class WeaponBow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2") && canFire)
+        ChargeFire();
+    }
+    void ChargeFire()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            holdStartTime = Time.time;
+            holding = true;
+        }
+        if (Input.GetButtonUp("Fire2"))
+        {
+            holding = false;
+        }
+        if (holding && Time.time - holdStartTime > holdTime)
         {
             FireBow();
-            StartCoroutine(Reload());
+            holding = false;
         }
     }
 
     void FireBow()
     {
-        canFire = false;
         GameObject projectileClone = Instantiate(projectile, transform.position, transform.rotation);
         Rigidbody rbody = projectileClone.GetComponent<Rigidbody>();
 
@@ -40,9 +55,4 @@ public class WeaponBow : MonoBehaviour
         Destroy(projectileClone, 5);
     }
 
-    IEnumerator Reload()
-    {
-        yield return new WaitForSeconds(fireSpeed);
-        canFire = true;
-    }
 }
