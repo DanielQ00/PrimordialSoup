b5420ad96210c78a8bcf4c3e10c0dd48fa79e65c
Jordan Rowland <46544691+rowlanddeep@users.noreply.github.com>
2021-01-31 14:03:31 -0500

    recenterd chest preb, AGAIN

---
+++ Assets/Scripts/ChestOpener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpener : MonoBehaviour
+{
+    BoxCollider coll;
+    Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponentInChildren<BoxCollider>();
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Chest opening");
+        a.SetTrigger("OpenChest");
+    }
+}
