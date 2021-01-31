583543dc5a677d789f1538c5cf8873a58d197359
Jordan Rowland <46544691+rowlanddeep@users.noreply.github.com>
2021-01-30 20:41:17 -0500

    Added Sword Animation

---
+++ Assets/Scripts/SwordAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAnim : MonoBehaviour
+{
+    public float attackSpeed = 0.3f;
+    private float lastSwing = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator a = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time - lastSwing > attackSpeed)
+        {
+            lastSwing = Time.time;
+            SwingSword();
+        }
+    }
+
+    void SwingSword()
+    {
+        a.SetTrigger("Swing");
+            
+        //Destroy(swordSwing, 0.3f);
+    }
+}
