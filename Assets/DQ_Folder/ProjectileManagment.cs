64250be68d4741709d54649bc1b6be9a8c2a5211
Daniel Quintana <daniquintana30@gmail.com>
2021-01-31 15:28:21 -0500

    Adding Changes
    Changes

--- Assets/DQ_Folder/ProjectileManagment.cs
+++ Assets/DQ_Folder/ProjectileManagment.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ProjectileManagment : MonoBehaviour
 {
+    public int damage =25;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<EnemyHealthRJD>())
         {
-
+            Debug.Log("Sending Damage");
+            collision.gameObject.GetComponent<EnemyHealthRJD>().TakeDamage(damage);
         }
         else
         {
             Debug.Log("Freezing");
         }
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        GetComponent<BoxCollider>().enabled = false;
     }
 }
